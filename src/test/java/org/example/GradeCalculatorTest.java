393ccd43b0f3d6bb284d45adffb1aedec460db4a
Eongyu Park <peg5325@gmail.com>
2023-05-15 17:11:21 +0900

    create calculateGrade test

--- src/test/java/org/example/GradeCalculatorTest.java
+++ src/test/java/org/example/GradeCalculatorTest.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * • 평균학점 계산 방법 = (학점수×교과목 평점)의 합계 / 수강신청 총학점 수
@@ -14,9 +19,15 @@ public class GradeCalculatorTest {
     // 이수한 과목을 전달하여 평균학점 계산 요청 ----> 학점 계산기 ----> (학점수×교과목 평점)의 합계 ----> 과목(코스)
     //                                               ----> 수강신청 총학점 수        ----> 과목(코스)
 
-
+    @DisplayName("평균 학점을 계산한다.")
     @Test
     void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"),
+                new Course("자료구조", 3, "A+"));
 
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
     }
 }
